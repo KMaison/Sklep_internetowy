444c94ef56bc88bdf9b0d9bbee4f0d8925568f8c
ani119 <karolina_makuch_email@wp.pl>
2019-08-24 22:43:29 +0200

    Revert "Define structure"
    This reverts commit e943b554688f5fba4733573707c7cb6634275f41.

--- WCFServiceWebRole1/Client.cs
+++ WCFServiceWebRole1/Client.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WCFServiceWebRole1.Domain
+namespace Client.Domain
 {
-    public class Client_order
+    public class Client
     {
+        public string Pesel { get; set; }
+        public string Firstname { get; set; }
+        public string Surname { get; set; }
         public string Order_ID { get; set; }
-        public string Address { get; set; }
-        public string Order_status { get; set; }
     }
 }
--- WCFServiceWebRole1/Product.cs
+++ WCFServiceWebRole1/Product.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WCFServiceWebRole1.Domain
+namespace Client.Domain
 {
     public class Product
     {
